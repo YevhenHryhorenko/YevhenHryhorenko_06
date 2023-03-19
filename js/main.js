067a07214e058ea03f67945b6df1f2ce0332d510
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-03-19 23:43:53 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let mark = 0
+let questions = [
+    'What`s 2 + 2 ? ',
+    'The sun rises in the east?',
+    'How much will be 5 / 0?',
+    'What color is the sky?',
+    'What is the correct answer to the ultimate question of life, the universe and all that?'
+]
+let answers = ["4", "true", "infinity", "blue", "42"]
+
+alert('All answers should be in English')
+
+for (let index = 0; index < questions.length; index++) {
+
+    if (index == 1) {
+        if (confirm(questions[index]) == answers[index]) {
+            mark += 10
+        }
+    } else {
+        if (prompt(questions[index]).toLowerCase() == answers[index]) {
+            mark += 10
+        }
+    }
+}
+
+alert(`Your mark is ${mark}`)
